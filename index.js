fd40a1f78dbfc46505ec923b5a2271074263f04f
nwe <nathan.ehrlich2621@gmail.com>
2021-08-30 13:16:49 -0600

    base file structure creation

---
+++ index.js
@@ -0,0 +1,77 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+function saveData(data) {
+    fs.appendFileSync('./Output/employee.json', data, 'utf-8');
+}
+
+function empType(){
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'employeeType',
+      message: 'Which type of team member would you like to add? (Use Arrow Keys)',
+      choices: ['Engineer', 'Intern','I am done entering new Employees'],
+      loop: false,
+    },
+  ])
+  .then((data) => {
+    const {employeeType} = data;
+    questions(employeeType);
+  })
+}
+
+function questions(type){
+    
+    if(type !== 'I am done entering new Employees'){
+          inquirer.prompt([
+        {
+          type: "input",
+          message: `What is the team ${type}'s name?`,
+          name: "name",
+        },
+        {
+            type: "input",
+            message: `What is the team ${type}'s name?`,
+            name: "id",
+        },
+        {
+          type: "input",
+          message: `What is the team ${type}'s name?`,
+          name: "email",
+        },
+        {
+          type: "input",
+          message: `What is the team ${type}'s office number?`,
+          name: "number",
+          when: (type) => type === 'Manager'
+        },
+        {
+          type: "input",
+          message: `What is the team ${type}'s office number?`,
+          name: "number",
+          when: (type) => type === 'Manager'
+        },
+        {
+          type: "input",
+          message: `What is the team ${type}'s office number?`,
+          name: "number",
+          when: (type) => type === 'Manager'
+        },
+      ])
+
+      .then((data) => {
+        // saveData(data);
+        empType();
+      })
+
+    } else {
+        //Create Files
+        console.log('Create HTML Data');
+    }
+}
+
+questions('Manager');
+
+
+
