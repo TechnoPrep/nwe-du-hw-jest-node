13749cbd697788efcd49689c7867f89106ede7a5
nwe <nathan.ehrlich2621@gmail.com>
2021-08-31 13:56:57 -0600

    Created 5 functions, create Cards for each emp type, a function to map over the object using a switch statement, and then createHTML to generate the HTML data. added module.export for the createHTML function

---
+++ src/createHTML.js
@@ -0,0 +1,140 @@
+// Set a blank string to append data to later
+let team = ``;
+
+const generateCards = (data) => {
+  //Loop over the array of objects passed from data
+
+  data.map((emp) => {
+    //Conditional to determine Employee Role
+    // Iterate over the array of objects and append the HTMl to the team string
+    switch (emp.getRole()) {
+      case "Manager":
+        team += createMgmt(emp);
+        break;
+      case "Engineer":
+        team += createEng(emp);
+        break;
+      case "Intern":
+        team += createInt(emp);
+        break;
+
+      default:
+        break;
+    }
+  });
+  return team;
+};
+
+const createMgmt = (data) => {
+  const { number, fullName, id, email } = data;
+
+  let cleanNum = formatNumber(number);
+
+  return `
+  <div class="card card-custom border-custom" style="width: 18rem;">
+        <div class="card-body top-border-custom bg-primary">
+          <h5 class="card-title text-white">${fullName}</h5>
+          <p class="card-text text-white">${data.getRole()}</p>
+        </div>
+        <div class="container p-3 bottom-border-custom bg-gray-custom">
+          <ul class=" bg-white list-group list-group-flush">
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">Email: ${email}</li>
+            <li class="list-group-item">Number: ${cleanNum}</li>
+          </ul>
+        </div>
+      </div>`;
+};
+
+const createEng = (data) => {
+  const { gitHub, fullName, id, email } = data;
+
+  return `
+  <div class="card card-custom border-custom" style="width: 18rem;">
+        <div class="card-body top-border-custom bg-primary">
+          <h5 class="card-title text-white">${fullName}</h5>
+          <p class="card-text text-white">${data.getRole()}</p>
+        </div>
+        <div class="container p-3 bottom-border-custom bg-gray-custom">
+          <ul class=" bg-white list-group list-group-flush">
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">Email: ${email}</li>
+            <li class="list-group-item">GitHub: ${gitHub}</li>
+          </ul>
+        </div>
+      </div>`;
+};
+
+// Create Intern Contact Card
+const createInt = (data) => {
+  const { school , fullName, id, email } = data;
+
+  return `
+  <div class="card card-custom border-custom" style="width: 18rem;">
+        <div class="card-body top-border-custom bg-primary">
+          <h5 class="card-title text-white">${fullName}</h5>
+          <p class="card-text text-white">${data.getRole()}</p>
+        </div>
+        <div class="container p-3 bottom-border-custom bg-gray-custom">
+          <ul class=" bg-white list-group list-group-flush">
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">Email: ${email}</li>
+            <li class="list-group-item">School: ${school}</li>
+          </ul>
+        </div>
+      </div>`;
+};
+
+const formatNumber = (number) => {
+
+  //RegEx to remove all non-numeric characters
+  // split into an array
+  let numArr = number.replace(/\D/g,'').split('');
+  
+  //Strip first number if it is a 1
+  if(numArr[0] === '1'){
+    numArr.shift();
+  }
+  
+  //Format numbers to be in the example format
+  //Exampe Format '(555) 555-5555'
+  numArr.splice(0,0,'(');
+  numArr.splice(4,0,') ');
+  numArr.splice(8,0,'-');
+
+  //Convert back to a string
+  clearNum = numArr.join('');
+
+  return clearNum;
+}
+
+//Run the createHTML fucntion to generate the Cards
+//Passing the responses from the Inquirer in index.js
+const createHTML = (data) => {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Document</title>
+</head>
+<body>
+
+  <header class='bg-red-custom p-4'>
+      <h1 class='text-center'>Team Card Builder</h1>
+      <h2 class='text-center'>Node JS</h2>
+  </header>
+
+  <main class='container p-4'>
+  ${generateCards(data)}
+  </main>
+</body>
+</html>`;
+};
+
+module.exports = createHTML;
